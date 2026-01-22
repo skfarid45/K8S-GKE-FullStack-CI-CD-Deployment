5493fe4f95da679010cc298d3a7cb901ad4dfaad
Shaik Farid <shaikfarid524@gmail.com>
2026-01-22 15:30:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/health', (req, res) => {
-  res.json({ message: 'Backend is running on GKE 🚀' });
+  res.json({ message: 'Backend is running on GKE ' });
 });
 
 app.listen(3000, () => {
